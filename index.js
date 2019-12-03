09380cff8044c07fc71e4f7324ed535e158ae4ba
I-Chiao Hsu <ichiao.hsu@gmail.com>
2019-12-03 15:55:10 +0800

    refactor(slack): moved slack related function to slack.js

--- index.js
+++ index.js
@@ -4,22 +4,7 @@
  */
 
 const users = require('./user-mapping.json')
-
-const { WebClient } = require('@slack/web-api')
-const slackToken = process.env.SLACK_TOKEN;
-const slack = new WebClient(slackToken)
-
-// Search through Slack user list for Slack id with GitHub user name
-const userByName = async (name) => {
-  let userId
-  try {
-    const reply = await slack.users.list()
-    userId = reply.members.find(x => x.name === name).id
-  } catch (err) {
-    console.error(err)
-  }
-  return userId
-}
+const { slack, userByName } = require('./lib/slack.js');
 
 module.exports = app => {
   
